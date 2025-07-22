5d36b6b5156330254004b946ecc209d122754f1f
asrulhadi <asrulhadi@teknomatrik.com.my>
2025-07-22 17:13:20 +0800

    Simple Program

--- Lesen/Program.cs
+++ Lesen/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+using DeviceId;
+using DeviceId.Encoders;
+using DeviceId.Formatters;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string deviceId = new DeviceIdBuilder()
+            .AddMachineName()
+            .AddOsVersion()
+            .UseFormatter(new HashDeviceIdFormatter(() => MD5.Create(), new HexByteArrayEncoder()))
+            .ToString();
+        Guid guid = new(deviceId);
+        Console.WriteLine($"device id = {deviceId} guid {guid}");
+    }
+}
